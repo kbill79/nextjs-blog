0128a0ae87645d0a1ded2c737409546eef624d13
Klebert Soares <klebert.soares@gmail.com>
2023-06-12 15:01:47 -0300

    Dynamic Routes

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
+// pages/home.js
 
+import React from 'react';
+import Head from 'next/head';
+import Layout from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+import Date from '../components/date';
 import { getSortedPostsData } from '../lib/posts';
+
+const HomeController = ({ allPostsData }) => {
+  return (
+    <Layout>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <HomeView allPostsData={allPostsData} />
+    </Layout>
+  );
+};
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -13,34 +28,42 @@ export async function getStaticProps() {
   };
 }
 
-export default function Home({ allPostsData }) {
+const HomeView = ({ allPostsData }) => {
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
-          ))}
-        </ul>
-      </section>
-
-    </Layout>
+    <section className={utilStyles.headingMd}>
+      <p>[Your Self Introduction]</p>
+      <p>
+        (This is a sample website - you’ll be building a site like this on{' '}
+        <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+      </p>
+      <BlogSection allPostsData={allPostsData} />
+    </section>
   );
-}
+};
+
+const BlogSection = ({ allPostsData }) => {
+  return (
+    <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      <h2 className={utilStyles.headingLg}>Blog</h2>
+      <ul className={utilStyles.list}>
+        {allPostsData.map(({ id, date, title }) => (
+          <BlogListItem key={id} id={id} date={date} title={title} />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+const BlogListItem = ({ id, date, title }) => {
+  return (
+    <li className={utilStyles.listItem} key={id}>
+  <Link href={`/posts/${id}`}>{title}</Link>
+  <br />
+  <small className={utilStyles.lightText}>
+    <Date dateString={date} />
+  </small>
+</li>
+  );
+};
+
+export default HomeController;
